a9eb9444d486303c58329b8497fbb9755237c788
AliSohani <a.sohani.shahrood@gmail.com>
2023-11-04 14:01:17 +0330

    there was a workflows folder in client for some fucking reason.... I deleted it

---
+++ client/.next/static/chunks/app/(auth)/layout-d972ab7772d5bfbd.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[695],{7017:function(e,n,s){Promise.resolve().then(s.t.bind(s,413,23)),Promise.resolve().then(s.t.bind(s,9167,23))}},function(e){e.O(0,[254,971,472,744],function(){return e(e.s=7017)}),_N_E=e.O()}]);
