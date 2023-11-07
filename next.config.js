ac124288ba42f9b3c2f0c60f6f941010e11778ff
AliSohani <a.sohani.shahrood@gmail.com>
2023-11-07 21:16:38 +0330

    added the new next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  rewrites: async () => {
+    return [
+      {
+        source: '/src/api/:path*',
+        destination:
+          process.env.NODE_ENV === 'development'
+            ? 'http://127.0.0.1:8000/api/:path*'
+            : '/api/',
+      },
+      {
+        source: 'src/docs',
+        destination:
+          process.env.NODE_ENV === 'development'
+            ? 'http://127.0.0.1:8000/docs'
+            : '/api/docs',
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
