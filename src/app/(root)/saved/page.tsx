463915a2733ffeea864fb9358e5ce6451251378f
AliSohani <a.sohani.shahrood@gmail.com>
2023-12-24 16:43:58 +0330

    this is nextjs + laravel

--- src/app/(root)/saved/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-type Props = {};
-
-const savedPage = (props: Props) => {
-  return <div>savedPage</div>;
-};
-
-export default savedPage;
--- src/components/shared/Loader.tsx
+++
@@ -1,9 +0,0 @@
-import { Loader2 as LoaderIcon } from 'lucide-react';
-
-const Loader = () => (
-  <div className="flex-center w-full animate-spin">
-    <LoaderIcon />
-  </div>
-);
-
-export default Loader;
