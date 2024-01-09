ee773c15824e06925709a3969c634e3b5db30a45
rezatb <103198863+rezatb@users.noreply.github.com>
2024-01-09 17:00:18 +0330

    Holla

--- api/functional_function/function.py
+++ api/functional_function/function.py
@@ -10,10 +10,17 @@ def ingredient_to_json(ingredient):
         "unit_type": ingredient.unit_type
     }
 
-conn = psycopg2.connect(database="Feast",
-                        host="localhost",
-                        user="postgres",
-                        password="0322262",
-                        port="5432")
+# conn = psycopg2.connect(database="Feast",
+#                         host="postgresql://AliSohani2082:oTsYDgB4P8Wp@ep-nameless-math-75218152.us-east-2.aws.neon.tech/Feast?sslmode=require",
+#                         user="AliSohani2082",
+#                         password="oTsYDgB4P8Wp")
 
+conn = psycopg2.connect(
+    host='ep-sweet-darkness-58939442.il-central-1.aws.neon.tech',
+    dbname='Feast',
+    user='rezatahmasbi1381',
+    password='tzkpv3ao5NPO',
+    sslmode='require',
+    port ='5432'
+)
 cursor = conn.cursor()
