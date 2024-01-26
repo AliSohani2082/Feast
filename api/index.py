ac3c4f7ad3c6c9f9fb643b727895ae0577ffb9ed
Alireza <lfrdwsy645@gmail.com>
2024-01-26 12:35:08 +0330

    Change uvicorn port

--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
 import uvicorn
 
 if __name__ == '__main__':
-    uvicorn.run('config:app', host='localhost', port=3000,reload=True)
+    uvicorn.run('config:app', host='localhost', port=8000,reload=True)
